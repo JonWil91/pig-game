3ab59de531e298e58cbe04810fa9c5576194f4c7
Jon Williams <jrwilliams100@hotmail.co.uk>
2021-01-04 15:35:40 +0000

    Added functionality to swap player when a 1 is rolled

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Selecting elements
-const player0El = document.querySelector('.player--0'); 
+const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1');
 // Player 0 and 1 instead of 1 and 2, as will be using zero index array positioning to select player score
 const score0El = document.querySelector('#score--0');
@@ -19,6 +19,8 @@ const btnHold = document.querySelector('.btn--hold');
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+let currentScore = 0;
+let activePlayer = 0;
 
 // Rolling dice funtionality
 btnRoll.addEventListener('click', function () {
@@ -27,4 +29,20 @@ btnRoll.addEventListener('click', function () {
     // 2. Display dice
     diceEl.classList.remove('hidden');
     diceEl.src = `static/dice-${dice}.png`;
+    // Check if dice roll is equal to 1
+    if (dice !== 1) {
+        // Add dice value to current score
+        currentScore += dice;
+        // Dynamically select the current active player and add to their current score
+        document.querySelector(`#current--${activePlayer}`).textContent = currentScore;
+    } else {
+        // Switch player
+        if (activePlayer === 0) {
+            activePlayer = 1;
+        } else {
+            activePlayer = 0;
+        }
+    }
 });
+
+
