b29aaf6838750ada5bb58cd4ed2eff879aa01541
Jon Williams <jrwilliams100@hotmail.co.uk>
2021-01-04 12:06:02 +0000

    Added random dice functionality

--- script.js
+++ script.js
@@ -13,3 +13,18 @@ const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
+
+// Starting conditions
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+// Rolling dice funtionality
+btnRoll.addEventListener('click', function () {
+    // 1. Generate random dice roll
+    const dice = Math.floor(Math.random() * 6) + 1;
+    // 2. Display dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `static/dice-${dice}.png`;
+});
